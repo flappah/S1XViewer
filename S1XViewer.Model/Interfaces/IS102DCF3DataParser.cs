1714a01d80a6c35eb3e115ca5d4301cce6d08f3c
Richard Flapper <rrj.flapper@mindef.nl>
2023-03-17 12:53:20 +0100

    Added boilerplate code for S102 dataparsers

---
+++ S1XViewer.Model/Interfaces/IS102DCF3DataParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1XViewer.Model.Interfaces
+{
+    public interface IS102DCF3DataParser : IHdfDataParserBase
+    {
+    }
+}
